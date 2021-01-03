ced0752fe589ac605ed662a6a0322bf2af8bed84
PhatFood <baophatnguyen99@gmail.com>
2021-01-03 23:05:42 +0700

    test init

---
+++ query-insert-data.sql
@@ -0,0 +1,2 @@
+insert into users (id,userName,userPassword,email,createdAt,updatedAt,lastLogin,roleId)
+            values (DEFAULT,'phucmanh1999','12345678','phucmanh1999@gmail.com',current_timestamp,current_timestamp,current_timestamp,3)
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const pg = require('pg')
 const port = process.env.PORT||3000
 const app = express()
 
-app.get('/', (req,res) =>{
-    res.send("Online Academy")
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('/index', (req,res) =>{
+    res.render('index')
 })
 
 app.listen(port,() => console.log("App available at http://localhost:3000"))
