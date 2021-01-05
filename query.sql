e315a02a2431b7a43ee59eeace7e8be5249cd3c3
PhatFood <baophatnguyen99@gmail.com>
2021-01-05 20:10:20 +0700

    fix database init qurery

--- query.sql
+++ query.sql
@@ -14,9 +14,9 @@
 
 CREATE TABLE roles ( -- admin, học viên, giảng viên
 	id serial PRIMARY KEY,
-	roleName VARCHAR ( 50 ) UNIQUE NOT NULL,
-	createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+	role_name VARCHAR ( 50 ) UNIQUE NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 -- Unlike VARCHAR, The CHARACTER or  CHAR without the length specifier (n) is the same as the CHARACTER(1) or CHAR(1).
@@ -28,131 +28,129 @@ CREATE TABLE roles ( -- admin, học viên, giảng viên
 
 CREATE TABLE users (
 	id serial PRIMARY KEY,
-	userName VARCHAR (255) UNIQUE NOT NULL,
-	userPassword VARCHAR (255) NOT NULL,
+	user_name VARCHAR (255) UNIQUE NOT NULL,
+	user_password VARCHAR (255) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-    firstName VARCHAR (255),
-    lastName VARCHAR (255),
+    first_name VARCHAR (255),
+    last_name VARCHAR (255),
     gender VARCHAR(1), --M: male, F: female
     birthday DATE,
-    avatarUrl VARCHAR(255),
-    userAddress VARCHAR(255),
-	createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP,
-    lastLogin TIMESTAMP,
-    roleId INTEGER NOT NULL REFERENCES roles(id)
+    avatar_url VARCHAR(255),
+    user_address VARCHAR(255),
+	created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    last_login TIMESTAMP,
+    role_id INTEGER NOT NULL REFERENCES roles(id)
 
 );
 -- 3 bảng update các cột sau :v
 CREATE TABLE student ( -- mua khóa học, đánh giá khóa học, danh sách khóa học yêu thích
 	id serial PRIMARY KEY,
-    userId INTEGER NOT NULL REFERENCES users(id),
-    buyNumber INTEGER -- Số khóa học đã mua
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    buy_number INTEGER -- Số khóa học đã mua
 
 );
 CREATE TABLE instructor (-- giảng viên: đăng khóa học
 	id serial PRIMARY KEY,
-    userId INTEGER NOT NULL REFERENCES users(id),
-    jobTitle VARCHAR(255), --ex: Professional Web Developer and Instructor
-    shortDescription TEXT,
-    fullDescription TEXT,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    job_title VARCHAR(255), --ex: Professional Web Developer and Instructor
+    short_description TEXT,
+    full_description TEXT,
     --videoPresentUrl VARCHAR(255),???
     rating NUMERIC,
-    ratingNumber INTEGER, -- so luong hoc vien danh gia
-    enrollNumber INTEGER, --so luong hoc vien dk hoc
-    courseNumber INTEGER -- so khoa hoc
+    rating_number INTEGER, -- so luong hoc vien danh gia
+    enroll_number INTEGER, --so luong hoc vien dk hoc
+    course_number INTEGER -- so khoa hoc
 
 );
 CREATE TABLE administrator (
 	id serial PRIMARY KEY,
-    userId INTEGER NOT NULL REFERENCES users(id)
+    user_id INTEGER NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE categories (
 	id serial PRIMARY KEY,
-	categoryName VARCHAR (255) UNIQUE NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+	category_name VARCHAR (255) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE courses (
 	id serial PRIMARY KEY,
-	courseName VARCHAR (255) UNIQUE NOT NULL,
-    imgPath VARCHAR(255),
-    shortDescription TEXT,
-    fullDescription TEXT,
+	course_name VARCHAR (255) UNIQUE NOT NULL,
+    img_path VARCHAR(255),
+    short_description TEXT,
+    full_description TEXT,
     --videoPresentUrl VARCHAR(255),???
     rating NUMERIC,
-    ratingNumber INTEGER, -- so luong hoc vien danh gia
-    enrollNumber INTEGER, --so luong hoc vien dk hoc
-    chapterNumber INTEGER, -- so chuong
-    viewNumber INTEGER, -- luot xem
+    rating_number INTEGER, -- so luong hoc vien danh gia
+    enroll_number INTEGER, --so luong hoc vien dk hoc
+    chapter_number INTEGER, -- so chuong
+    view_number INTEGER, -- luot xem
     price NUMERIC,
     concurrency VARCHAR(50),
     discount SMALLINT,
-    courseLanguage VARCHAR(50),
-    courseState VARCHAR(1), --F: Finished, U: Unfinished
-	createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP,
-    categoryId INTEGER NOT NULL REFERENCES categories(id),
-    instructorId INTEGER NOT NULL REFERENCES instructor(id)
+    course_language VARCHAR(50),
+    course_state VARCHAR(1), --F: Finished, U: Unfinished
+	created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    category_id INTEGER NOT NULL REFERENCES categories(id),
+    instructor_id INTEGER NOT NULL REFERENCES instructor(id)
 );
 
 CREATE TABLE chapters( -- một khóa học có nhiều chương 1 2 3 ...
     id serial PRIMARY KEY,
-    courseId INTEGER NOT NULL REFERENCES courses(id),
-    lessonNumber INTEGER,
-    shortDescription TEXT,
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    course_id INTEGER NOT NULL REFERENCES courses(id),
+    lesson_number INTEGER,
+    short_description TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE lessons ( -- mỗi chương có nhiều bài 1 2 3 ...
 	id serial PRIMARY KEY,
-    chapterId INTEGER NOT NULL REFERENCES chapters(id),
-	lessonName VARCHAR (255) UNIQUE NOT NULL,
-    videoUrl VARCHAR(255),
-    shortDescription TEXT,
-    fullDescription TEXT,
-    viewNumber INTEGER, -- luot xem
+    chapter_id INTEGER NOT NULL REFERENCES chapters(id),
+	lesson_name VARCHAR (255) UNIQUE NOT NULL,
+    video_url VARCHAR(255),
+    short_description TEXT,
+    full_description TEXT,
+    view_number INTEGER, -- luot xem
     hour INTEGER,
-    currentWatch TIMESTAMP, -- xem tới khúc nào của video
-	createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    current_watch TIMESTAMP, -- xem tới khúc nào của video
+	created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE watchList( -- học viên: danh sach yêu thích: nhiều nhiều student-courses
     id serial PRIMARY KEY,
-    studentID INTEGER NOT NULL REFERENCES student(id),
-    courseId INTEGER NOT NULL REFERENCES courses(id),
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    student_id INTEGER NOT NULL REFERENCES student(id),
+    course_id INTEGER NOT NULL REFERENCES courses(id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE review( -- học viên đánh giá khóa học: nhiều - nhiều
     id serial PRIMARY KEY,
     comment TEXT,
     star INTEGER, -- từ 0-5 sao
-    studentID INTEGER NOT NULL REFERENCES student(id),
-    courseId INTEGER NOT NULL REFERENCES courses(id),
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    student_id INTEGER NOT NULL REFERENCES student(id),
+    course_id INTEGER NOT NULL REFERENCES courses(id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE cart(-- giỏ hàng: học viên mua khóa học: nhiều - nhiều
     id serial PRIMARY KEY,
-    studentID INTEGER NOT NULL REFERENCES student(id),
-    courseId INTEGER NOT NULL REFERENCES courses(id),
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    student_id INTEGER NOT NULL REFERENCES student(id),
+    course_id INTEGER NOT NULL REFERENCES courses(id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE courseBought(-- khóa học đã đc mua:  nhiều - nhiều
     id serial PRIMARY KEY,
-    studentID INTEGER NOT NULL REFERENCES student(id),
-    courseId INTEGER NOT NULL REFERENCES courses(id),
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP
+    student_id INTEGER NOT NULL REFERENCES student(id),
+    course_id INTEGER NOT NULL REFERENCES courses(id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
-
-
