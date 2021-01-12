6beb42b5b1f296927d0380ae6d0a4463711e722c
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2021-01-12 10:39:00 +0700

    finish editCourse.ejs

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 const database = new Sequelize({
-    database: "online-academy",
+    database: "online-academy1",
     username: "postgres",
     password: "123456",
     host: "localhost",
