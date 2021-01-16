9b863a53b4a1414af001f3f5a7752134d5b57ae2
PhatFood <baophatnguyen99@gmail.com>
2021-01-16 23:40:39 +0700

    edit course, edit chapter, edit course api

--- services/lesson-service.js
+++ services/lesson-service.js
@@ -3,6 +3,19 @@ const createLesson = obj => {
     return Lesson.create(obj);
 }
 
+const updateLesson = (_id, obj) => {
+    return Lesson.update(
+        obj
+        ,
+        {
+            where: {
+                id: _id
+            },
+        }
+    )
+}
+
 module.exports = {
     createLesson,
+    updateLesson
 }
