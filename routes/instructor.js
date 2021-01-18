209678c298f60e53fdedb13f3ef035b36a78c523
PhatFood <baophatnguyen99@gmail.com>
2021-01-18 12:08:35 +0700

    fix instructor api

--- routes/instructor.js
+++ routes/instructor.js
@@ -256,7 +256,7 @@ router.post('/addCourse', urlencodedParser, async (req, res) => {
                     imageFile.mv("./public/assets/images/" + imageFile.name)
                 getInstructor({id: instructorId}).then(ins => {
                     updateInstructor(instructorId, {
-                        course_number: ins.course_number ? 1 : ins.course_number + 1;
+                        course_number: ins.course_number ? 1 : ins.course_number + 1
                     })
                 })
                 res.json({msg: "ok"})
