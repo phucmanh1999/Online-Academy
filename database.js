3cb58c8a558781d7813a325e1940d918b9a8ddb6
Nguyen Phuc Manh <phucmanh1999@gmail.com>
2021-01-19 00:22:39 +0700

    fix url save

--- database.js
+++ database.js
@@ -1,16 +1,5 @@
 const {Sequelize} = require('sequelize')
 
-// const database = new Sequelize({
-//     database: "online-academy",
-//     username: "postgres",
-//     password: "123456",
-//     host: "localhost",
-//     port: 5432,
-//     dialect: "postgres",
-//     logging: false
-// })
-
-
 const database = new Sequelize({
     database: "online-academy1",
     username: "postgres",
@@ -18,13 +7,24 @@ const database = new Sequelize({
     host: "localhost",
     port: 5432,
     dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-        ssl: {
-          require: true, // This will help you. But you will see nwe error
-          rejectUnauthorized: false // This line will fix new error
-        }
-      },
-    })
+    logging: false
+})
+
+
+// const database = new Sequelize({
+//     database: "online-academy1",
+//     username: "postgres",
+//     password: "123456",
+//     host: "localhost",
+//     port: 5432,
+//     dialect: "postgres",
+//     logging: false,
+//     dialectOptions: {
+//         ssl: {
+//           require: true, // This will help you. But you will see nwe error
+//           rejectUnauthorized: false // This line will fix new error
+//         }
+//       },
+//     })
 
 module.exports = database
