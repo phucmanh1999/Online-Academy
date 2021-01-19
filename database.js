2869b30705c952fd24450e0d513db6a9544054ee
PhatFood <baophatnguyen99@gmail.com>
2021-01-20 04:26:03 +0700

    setting database for heroku

--- database.js
+++ database.js
@@ -1,31 +1,31 @@
 const {Sequelize} = require('sequelize')
 
-const database = new Sequelize({
-    database: "online-academy",
-    username: "postgres",
-    password: "1",
-    host: "localhost",
-    port: 5432,
-    dialect: "postgres",
-    logging: false
-})
-
-
 // const database = new Sequelize({
-//     database: "d5968uq2v63ter",
-//     username: "jvlbpogybkrtne",
-//     password: "d3b92cccae06ed08d402a7a423b7f2cc65b1c01170d874db2892f444f0f6de6b",
-//     host: "ec2-52-22-135-159.compute-1.amazonaws.com",
+//     database: "online-academy",
+//     username: "postgres",
+//     password: "1",
+//     host: "localhost",
 //     port: 5432,
 //     dialect: "postgres",
-//     logging: false,
-//     dialectOptions: {
-//         ssl: {
-//           require: true, // This will help you. But you will see nwe error
-//           rejectUnauthorized: false // This line will fix new error
-//         }
-//       },
-//     })
+//     logging: false
+// })
+
+
+const database = new Sequelize({
+    database: "d5968uq2v63ter",
+    username: "jvlbpogybkrtne",
+    password: "d3b92cccae06ed08d402a7a423b7f2cc65b1c01170d874db2892f444f0f6de6b",
+    host: "ec2-52-22-135-159.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
+      },
+    })
 
 
 // const database = new Sequelize({
